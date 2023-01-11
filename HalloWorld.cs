ebe27340d14762dd6571b8fe1d1a08a488248d85
Pashot123 <122459310+Pashot123@users.noreply.github.com>
2023-01-11 17:02:34 +0300

    scropt "HalloWorld"

---
+++ HalloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Выводит привет мир
+        print("Hallo World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
